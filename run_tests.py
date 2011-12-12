bc4a77b559e6ba109016b4ac723b449d159efe62
nilin56 <nilin56@gmail.com>
2011-12-12 21:04:39 +0800

    print out

--- run_tests.py
+++ run_tests.py
@@ -3,4 +3,4 @@ import urllib, json
 url = 'http://172.16.162.11:9003/mapreduce/test_job?request='
 jobs = {'test_wiki_read.py' : 10}
 jobs_str = json.dumps(jobs)
-urllib.urlopen(url+jobs_str).read()
+print urllib.urlopen(url+jobs_str).read()
